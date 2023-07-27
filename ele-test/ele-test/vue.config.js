abb70d1dac460d2670295db3a0c24ab894ebcc26
gy <709970556@qq.com>
2023-07-27 23:04:18 +0800

    0.0.0.1

---
+++ ele-test/ele-test/vue.config.js
@@ -0,0 +1,25 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+    transpileDependencies: true,
+    devServer: {
+        historyApiFallback: true,
+        client: {
+            overlay: false,  // 禁用错误覆盖
+        },
+
+        proxy: {
+            '/api': {
+                //target: "http://localhost:5000",
+                //target: "http://127.0.0.1:4523/m1/3026709-0-default",
+                target: "http://124.70.7.210:5000",
+                changeOrigin: true,
+            },
+            '/commodity_image': {
+                //target: "http://localhost:5000",
+                //target: "http://127.0.0.1:4523/m1/3026709-0-default",
+                target: "http://124.70.7.210:5000",
+                changeOrigin: true
+            }
+        }
+    }
+})
