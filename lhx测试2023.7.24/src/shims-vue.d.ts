482ed5a0c0dc3967745184ce14dd14e16148f76b
gy <709970556@qq.com>
2023-07-27 23:03:17 +0800

    0.0.0.1

--- lhx测试2023.7.24/src/shims-vue.d.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable */
-
-//declare module '*.ts'
-declare module '*.vue' {
-  import { defineComponent } from 'vue'
-  const Component: ReturnType<typeof defineComponent>
-  export default Component
-}
-
