069d7487b35162d8a1134b90ecac1399168f2255
gy <709970556@qq.com>
2023-09-04 01:00:28 +0800

    0.0.2.0

--- ver1.9/ver1.9/frontend/router/baseURL.js
+++
@@ -1,6 +0,0 @@
-const baseURL="http://localhost:5000"
-//const baseURL="http://127.0.0.1:4523/m1/3026709-0-default"
-//const baseURL="http://124.70.7.210:5000"
-//在此处，切换本地服务器，mock以及远程服务器
-
-export default baseURL;
